bef86f7c9a212dd48f3fdf6e79797ea525940887
Caroline Meeks <caroline@meekshome.com>
2014-02-21 14:15:00 -0500

    First audio

--- client/play.js
+++ client/play.js
@@ -3,5 +3,10 @@ Template.play.helpers ({
 	    var src = 'clock.png';
 	    var image = '<img src="' + src + '" id="play-img" width="40%">';
 	    return image;
+	},
+	audio: function() {
+	    var soundFile ="LakeIsleofInnisfree.mp3"
+	    var audio = '<span ><audio id="audio" src="'+ soundFile + '" hidden="true" autostart="true" loop="false" /></span>'
+	    return audio
 	}
 });
