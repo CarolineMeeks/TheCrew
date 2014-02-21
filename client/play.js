f736a8b2630e0e3790affb85837cc5bb2e0b35cd
Caroline Meeks <caroline@meekshome.com>
2014-02-21 14:47:55 -0500

    first timer

--- client/play.js
+++ client/play.js
@@ -1,12 +1,21 @@
 Template.play.helpers ({
-	image:  function () {
-	    var src = 'clock.png';
-	    var image = '<img src="' + src + '" id="play-img" width="40%">';
-	    return image;
-	},
-	audio: function() {
-	    var soundFile ="LakeIsleofInnisfree.mp3"
-	    var audio = '<span ><audio id="audio" src="'+ soundFile + '" hidden="true" autostart="true" loop="false" /></span>'
-	    return audio
-	}
+    image:  function () {
+	var src = 'clock.png';
+	var image = '<img src="' + src + '" id="play-img" width="40%">';
+	return image;
+    },
+    audio: function() {
+	var soundFile ="LakeIsleofInnisfree.mp3"
+	var audio = '<span ><audio id="audio" src="'+ soundFile + '" hidden="true" loop="false" /></span>'
+	return audio
+    },
+    timer: function() {
+	var time = 30;
+	var timer = '<div id="timer" data-timer="' + time + '"></div>'
+	return timer
+    }
 });
+
+Template.play.rendered = function() {
+    $("#timer").TimeCircles();
+}; 
