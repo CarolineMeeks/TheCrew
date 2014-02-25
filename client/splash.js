5b12fe021c6cc92778fed1b631d14e85f7f2dcd7
Caroline Meeks <caroline@meekshome.com>
2014-02-25 07:08:41 -0500

    check point before phonegap

--- client/splash.js
+++ client/splash.js
@@ -1,11 +1,12 @@
 Template.splash.helpers ({
 	message:  function () {
 	    var message = ""
-	    if (Meteor.userId()) {
-		message = "The Crew <br> Click to Enter"
-	    } else {
-		message = "Login with Facebook to Proceed"
-	    };
+	    message = "The Crew <br> Click to Enter"
+	   // if (Meteor.userId()) {
+//		message = "The Crew <br> Click to Enter"
+//	    } else {
+//		message = "Login with Facebook to Proceed"
+//	    };
 	    return message;
 	}
 });
@@ -14,9 +15,16 @@ Template.splash.events({
     'click #splashimg' : function (e) {
 	e.preventDefault();
 	console.log('splash clicked');
-	if (Meteor.userId()) {
+//	if (Meteor.userId()) {
 	    window.location = "/play";
-	};
+//	};
+    },
+    'click #greeting' : function (e) {
+	e.preventDefault();
+	console.log('splash clicked');
+//	if (Meteor.userId()) {
+	    window.location = "/play";
+//	};
     }
 });
 
