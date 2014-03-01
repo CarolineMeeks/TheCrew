6f55138c05820110131f5dc992f6cfc00a4dcd7e
Caroline Meeks <caroline@meekshome.com>
2014-03-01 16:48:56 -0500

    check point

--- server/flickr.js
+++ server/flickr.js
@@ -5,11 +5,15 @@ myFavList = function(apiKey, userId) {
     Meteor.http.call("GET", "http://api.flickr.com/services/rest/?method=flickr.favorites.getPublicList&api_key="+apiKey+"&user_id="+userId+"&extras=tags&format=json&nojsoncallback=1", {},function(error, result) {
 	console.log(result.statusCode);
 	if (result.statusCode == 200) {
-	    console.log(result.content);
+//	    console.log(result.content);
 	    var favsResult = JSON.parse(result.content);
+	    console.log(favsResult.photos.photo[1]);
 	    return favsResult;
 	};
     });
 };
 
 myFavs = myFavList(flickrKey, myFlickrId);
+console.log(myFavs.photos.photo[1]);
+//randomPhoto = Math.floor(Math.random()*(myFavs.photos.total));
+
